4fdd8537a9c9cd4b6590924513558320e442322d
DESKTOP-SJTNIIR\toan <thtoan159@gmail.com>
2022-12-12 23:41:12 +0700

    em đã làm 1 ít và em đang sửa lại các module để hoàn chỉnh 1 page

---
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-three_column.blade.php
@@ -0,0 +1,36 @@
+<section class="module-three-column">
+    <div class="container module-three-column">
+        <div class="module-page-three flex">
+            <div class="page-three-column ">
+                <div class="image-three-column-1">
+                    <img class="inline " src="@asset('./images/default/Mask Group 37.png')">
+                    <div><span>Process</span></div>
+                </div>
+                <div class="image-three-column-2">
+                    <img class="inline" src="@asset('./images/default/Mask Group 38.png')">
+                    <div><span>People</span></div>
+                </div>
+                <div class="image-three-column-3">
+                    <img class="inline" src="@asset('./images/default/Mask Group 39.png')">
+                    <div><span>Technology</span></div>
+                </div>
+                <div class="image-three-column-4">
+                    <img class="inline" src="@asset('./images/default/Mask Group 40.png')">
+                    <div><span>Data</span></div>
+                </div>
+            </div>
+            <div class="page-three-column-1 ">
+                <div class="content-column">
+                    <span>Connecting Your Business to What Matters Most</span>
+                </div>
+                <div class="content-column-at-bottom">
+                    <span>At Encompass, we are committed to improving the way the beverage industry works.
+                        How people make decisions, how they serve customers, and how they run their businesses—we bring it all together.
+                        Our cloud-native, secure, and compliant solutions are optimized for continuous innovation. Our ERP solutions unlock
+                        business growth opportunities by enabling each tier of the supply chain to drive operational efficiencies, improve how
+                        people work, and meet fast-changing, unpredictable consumer demand.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- wp-content/themes/sage-theme/resources/views/template-parts/page/module-two_column.blade.php
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-two_column.blade.php
@@ -10,7 +10,7 @@
                 simplify decision making, and equip businesses for success in a fast-changing and unpredictable market.</span>
         </div>
 
-        <div class="module-row-3 row w-full">
+        <div class="module-row-3 row ">
             <div class=" w-1/5  text-center"><img class="inline" src="@asset('./images/default/icon.png')"></div>
             <div class=" w-1/5 pt-8 text-center"><img class="inline" src="@asset('./images/default/Group.png')"></div>
             <div class=" w-1/5 text-center"><img class="inline" src="@asset('./images/default/Path.png')"></div>
@@ -18,20 +18,23 @@
             <div class=" w-1/5 pt-8 text-center"><img class="inline" src="@asset('./images/default/store-line.png')"></div>
         </div>
 
-        <div class="module-row-4 row w-full ">
-            <span class="module-1 w-1/3 pr-50">Solutions for Manufacturers</span>
-            <span class="module-1 w-1/3">Solutions for Distributors</span>
-            <span class="module-1 w-1/3  float">Solutions for Retailers</span>
+        <div class="module-row-4 row w-full">
+            <span class="module-1 w-1/3 ">Solutions for Manufacturers</span>
+            <span class="module-2 w-1/3 ">Solutions for Distributors</span>
+            <span class="module-3 w-1/3  ">Solutions for Retailers</span>
 
-            <span class="module-row w-1/3">Integrate all aspects of your operations with a single source of truth
+            <span class=" w-1/3 pl-15 pr-60 text-center mb-10 font-light">Integrate all aspects of your operations with a single source of truth
                 for planning, purchasing, production, QC, inventory, sales, accounting, finance, and business intelligence.</span>
-            <span class="module-column-1 w-1/3">Increase operational efficiency and manage growth effectively with products like warehouse management, sales execution, logistics,
+            <span class=" w-1/3 pl-50 pr-48 text-center mb-10 font-light">Increase operational efficiency and manage growth effectively with products like warehouse management, sales execution, logistics,
                 warehouse automation, retailer ordering, business intelligence, and more.</span>
-            <span class="module-column-1 w-1/3 pr-10">Run a more efficient business by optimizing interactions with your distributors and customers with solutions like online ordering,
+            <span class=" w-1/3 pl-85 pr-15 text-center font-light mb-10">Run a more efficient business by optimizing interactions with your distributors and customers with solutions like online ordering,
                 retail eCommerce websites, delivery applications, and more.</span>
-            <span class="module-column-3 w-1/3">EXPLORE ORCHESTRA</span>
-            <span class="module-column-3 w-1/3">EXPLORE encompass cloud</span>
-            <span class="module-column-3 w-1/3">EXPLORE retail solutions</span>
+            <span class="text-module-two">EXPLORE ORCHESTRA
+                <span class="icomoon icon-chevron-down"></span></span>
+            <span class="text-module-two-1  ">EXPLORE encompass cloud
+                <span class="icomoon icon-chevron-down"></span></span>
+            <span class="text-module-two-2  ">EXPLORE retail solutions
+                <span class="icomoon icon-chevron-down"></span></span>
         </div>
     </div>
 </section>
